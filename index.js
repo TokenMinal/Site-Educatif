edbe1074637f2810dbdfe4eb60e8ba32480e5846
TokenMinal <ludocrashnadeau@gmail.com>
2021-03-12 12:14:43 -0500

    second page/math number

--- index.js
+++ index.js
@@ -2,80 +2,166 @@ const select = document.getElementById('selection');
 const select2 = document.getElementById('selection2');
 const numbertest = document.getElementById('numbertest');
 const operatortest = document.getElementById('operatortest');
+const button = document.getElementById('button');
+const multiple1 = document.getElementsByClassName('multiple');
+const menu = document.getElementById('menu');
+const math = document.getElementById('math');
 let check;
 let checkop;
+let clicked = false;
 select.addEventListener('click', function() {
 	const x = document.getElementById('selection').value;
-	switch(x) {
-	case 'i1':
-		numbertest.textContent = '1';
-		check = false;
-		break;
-	case 'i2':
-
-		numbertest.textContent = '2';
-		check = false;
-		break;
-	case 'i3':
-
-		numbertest.textContent = '3';
-		check = false;
-		break;
-	case 'i4':
-
-		numbertest.textContent = '4';
-		check = false;
-		break;
-	case 'i5':
-
-		numbertest.textContent = '5';
-		check = false;
-		break;
-	case 'i6':
-
-		numbertest.textContent = '6';
-		check = false;
-		break;
-	case 'i7':
-
-		numbertest.textContent = '7';
-		check = false;
-		break;
-	case 'i8':
-
-		numbertest.textContent = '8';
-		check = false;
-		break;
-	case 'i9':
-
-		numbertest.textContent = '9';
-		check = false;
-		break;
-	case 'i10':
-
-		numbertest.textContent = '10';
-		check = false;
-		break;
-	case 'i11':
-
-		numbertest.textContent = '11';
-		check = false;
-		break;
-	case 'i12':
-		numbertest.textContent = '12';
-		check = false;
-		break;
-	case 'random':
-		if (check == true) {
+	if (clicked === false) {
+		switch(x) {
+		case 'i1':
+			numbertest.textContent = '1';
 			check = false;
 			break;
+		case 'i2':
+			numbertest.textContent = '2';
+			check = false;
+			break;
+		case 'i3':
+			numbertest.textContent = '3';
+			check = false;
+			break;
+		case 'i4':
+			numbertest.textContent = '4';
+			check = false;
+			break;
+		case 'i5':
+			numbertest.textContent = '5';
+			check = false;
+			break;
+		case 'i6':
+			numbertest.textContent = '6';
+			check = false;
+			break;
+		case 'i7':
+			numbertest.textContent = '7';
+			check = false;
+			break;
+		case 'i8':
+			numbertest.textContent = '8';
+			check = false;
+			break;
+		case 'i9':
+			numbertest.textContent = '9';
+			check = false;
+			break;
+		case 'i10':
+			numbertest.textContent = '10';
+			check = false;
+			break;
+		case 'i11':
+			numbertest.textContent = '11';
+			check = false;
+			break;
+		case 'i12':
+			numbertest.textContent = '12';
+			check = false;
+			break;
+		case 'random':
+			if (check == true) {
+				check = false;
+				break;
+			}
+			numbertest.textContent = Math.floor(Math.random() * 12) + 1;
+			check = true;
+			break;
+		default:
+			numbertest.textContent = ' aucun choisi';
 		}
-		numbertest.textContent = Math.floor(Math.random() * 12) + 1;
-		check = true;
-		break;
-	default:
-		numbertest.textContent = ' aucun choisi';
 	}
+	else if (clicked === true) {
+		let i;
+		switch(x) {
+		case 'i1':
+			for (i = 0; i <= 12; i++) {
+				multiple1[i].textContent = '1';
+			}
+			break;
+		case 'i2':
+			for (i = 0; i <= 12; i++) {
+				multiple1[i].textContent = '2';
+			}
+			check = false;
+			break;
+		case 'i3':
+			for (i = 0; i <= 12; i++) {
+				multiple1[i].textContent = '3';
+			}
+			check = false;
+			break;
+		case 'i4':
+			for (i = 0; i <= 12; i++) {
+				multiple1[i].textContent = '4';
+			}
+			check = false;
+			break;
+		case 'i5':
+			for (i = 0; i <= 12; i++) {
+				multiple1[i].textContent = '5';
+			}
+			check = false;
+			break;
+		case 'i6':
+			for (i = 0; i <= 12; i++) {
+				multiple1[i].textContent = '6';
+			}
+			check = false;
+			break;
+		case 'i7':
+			for (i = 0; i <= 12; i++) {
+				multiple1[i].textContent = '7';
+			}
+			check = false;
+			break;
+		case 'i8':
+			for (i = 0; i <= 12; i++) {
+				multiple1[i].textContent = '8';
+			}
+			check = false;
+			break;
+		case 'i9':
+			for (i = 0; i <= 12; i++) {
+				multiple1[i].textContent = '9';
+			}
+			check = false;
+			break;
+		case 'i10':
+			for (i = 0; i <= 12; i++) {
+				multiple1[i].textContent = '10';
+			}
+			check = false;
+			break;
+		case 'i11':
+			for (i = 0; i <= 12; i++) {
+				multiple1[i].textContent = '11';
+			}
+			check = false;
+			break;
+		case 'i12':
+			for (i = 0; i <= 12; i++) {
+				multiple1[i].textContent = '12';
+			}
+			check = false;
+			break;
+		case 'random':
+			if (check == true) {
+				check = false;
+				break;
+			}
+			for (i = 0; i <= 12; i++) {
+				multiple1[i].textContent = Math.floor(Math.random() * 12) + 1;
+			}
+			check = true;
+			break;
+		default:
+			numbertest.textContent = ' aucun choisi';
+		}
+	}
+
 });
 select2.addEventListener('click', function() {
 	const x = document.getElementById('selection2').value;
@@ -130,4 +216,9 @@ select2.addEventListener('click', function() {
 	default:
 		operatortest.textContent = ' Aucun choisi';
 	}
+});
+button.addEventListener('click', function() {
+	clicked = true;
+	menu.style.display = 'none';
+	math.style.display = 'inline';
 });
